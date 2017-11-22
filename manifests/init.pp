cf907d1c2ad92fcd0f61d02030ff174885c47ade
panoq <lauri.olavi.alho@gmail.com>
2017-11-22 22:01:20 +0200

    Added .bashrc file and changed the module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,9 @@ class drdes {
         file {"/usr/share/xfce4/backdrops/xubuntu-wallpaper.png":
                 content=>template("drdes/xubuntu-wallpaper.png"),
         }
+	file {'/home/xubuntu/.bashrc':
+		content=>template("drdes/.bashrc"),
+	}
 }
 
 
